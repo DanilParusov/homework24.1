1171a4d55c7175dd7c20eaed78723eebf0cb2065
DanilParusov <parusovdanik@gmail.com>
2023-09-29 23:20:28 +0200

    homework 25.1 in progress

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,13 @@ from django.urls import path, include
 from rest_framework import routers
 from . import views
 from .views import UserCreateApiView
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('create/', UserCreateApiView.as_view(), name='user_create'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
